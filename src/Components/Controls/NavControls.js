84c9c1e07af5ec1973b3a01edaef98998a6d9a60
Diazu27 <AndresDiazu2001@gmail.com>
2022-01-12 20:39:34 -0600

    Added ProductModal state

--- src/Components/Controls/NavControls.js
+++ src/Components/Controls/NavControls.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ModalProduct from '../ModalProduct/ModalProduct';
 
 const NavControls = () => {
+
+    const [Modal, setModal] = useState(false);
+
+
+    const handleModal = ()=>{
+        setModal(!Modal);
+        console.log(Modal);
+    }
+
+
+
     return (
         <div className='NavControl'>
-
             <div className='SearchBox'>
                 <input type="text"></input>
                 <button>Buscar</button>
             </div> 
-            
-            <button className='btnAdd'>Agregar</button>
+    
+            <button className='btnAdd' onClick={handleModal} >Agregar</button>
+            {Modal ? <ModalProduct handleClose={handleModal} /> : <></>}
         </div>
     )
 }
