6a1992b722673074ecdd2531413f759d56bea280
Diazu27 <AndresDiazu2001@gmail.com>
2022-01-12 18:33:56 -0600

    Add component and styles for table

---
+++ src/Components/Controls/Table.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const Table = () => {
+
+    var prod =["Pollo", "Res", "Fresas", "Papaya", "Aguacate", "limones", "Mangos", "cebollas", "Nance", "Pan Molde","Platano",
+    "Pollo", "Res", "Fresas", "Papaya", "Aguacate", "limones", "Mangos", "cebollas", "Nance", "Pan Molde","Platano"];
+
+    return (
+       <div className='tableCont'>
+        <table>
+            <thead>
+                <th>id</th>
+                <th>Producto</th>
+                <th>Catergoria</th>
+                <th>Cantidad</th>
+                <th>Precio</th>
+                <th>Opciones</th>
+            </thead>
+
+            <tbody>
+              {
+                  prod.map((Producto, index)=>{
+                    return(
+                        <tr>
+                        <td>1</td>
+                        <td>{Producto}</td>
+                        <td>Carnes</td>
+                        <td>200</td>
+                        <td>400.00</td>
+                        <td className='buttonbox'>
+                            <button className='btn-edit'>Editar</button>
+                            <button className='btn-delete'>Eliminar</button>
+                        </td>
+                        </tr>
+                    );
+                  })
+              }
+              
+            </tbody>
+
+
+
+        </table>
+       </div>
+    )
+}
+
+export default Table
